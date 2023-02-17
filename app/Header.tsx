d487e6db4e2a1821645ee8c8fcf8e4136834cb21
eloenwa <eloenwa@users.noreply.github.com>
2023-02-16 18:54:36 -0600

    work on mainpage
    This commit covers the work done on the mainpage. Work include
        - styling with chakra UI elements and populating the page
    Work pending on this page for now
        - Include the Tao sign next to 0.00 to show credit remaining (in Header.tsx)
        - Updating the font to match adobe XD design
        - Spacing btw the access btn and Box elements above

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,13 +1,23 @@
-import {Box } from '@chakra-ui/react';
-import React from 'react'
+import { Box, Image, HStack } from '@chakra-ui/react';
+import React from 'react';
+
+
+//this component will be called on very page 
+//it will use useState to conditional render the settings icon if the user is logged in
+//also useState will be used to get the users tor credit 
 
 function Header() {
   return (
     <header >
-        <Box h={'5vh'} bgColor={'#030B19'}
-        p={10}
-        color={'#FFFFFF'}
-        >Logo goes here</Box>
+        <Box h={'5vh'} bgColor={'#030B19'} pt={10} pl={10} pr={10}  color={'#FFFFFF'}>
+          {/*@ts-ignore the width and height were altered from the design to appear appropriate*/}          
+          <HStack justifyContent={'space-between'} >
+            <Image src='/chattensor.png' w={'120px'} h={'18px'}/>
+            <Box>0.00</Box>
+          </HStack>
+          
+          
+        </Box>
     </header>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { Box, HStack, VStack,Button } from '@chakra-ui/react';
+import { Box, HStack, VStack,Button, Text } from '@chakra-ui/react';
 import Header from './Header';
+import Link from 'next/link';
 
 //this is the main page users will be routed here on page load. 
 
@@ -13,13 +14,31 @@ export default function Home() {
       color={'#FFFFFF'}>
       {/* @ts-ignore  this is here because tyscript throws error lines for chakra components like VStack */}
       <VStack>
-        <HStack>
-          <Box h={'200px'} w={'187px'}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,  nibh.  </Box>
-          <Box h={'200px'} w={'187px'}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,  nibh.  </Box>
+        <HStack fontSize={'12px'} w={'394px'} justifyContent={'space-between'}>
+          <Box h={'200px'} w={'187px'} borderWidth={"0.5px"} borderColor={'#707070'} borderRadius={'5px'} >
+            {/* @ts-ignore  this is here because tyscript throws error lines for chakra components like VStack */}
+            <VStack>
+              <Text fontSize={'15px'} p={1}>Capabilities</Text>
+              <VStack fontFamily={'sans-serif arial'} lineHeight= '15px'>
+                <Text pl={'11px'} pr={'11px'}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Text>
+                <Text pl={'11px'} pr={'11px'}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Text>
+                <Text pl={'11px'} pr={'11px'}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Text>
+              </VStack>
+            </VStack>
+          </Box>
+          <Box h={'200px'} w={'187px'} borderWidth={"0.5px"} borderColor={'#707070'} borderRadius={'5px'} >
+            {/* @ts-ignore  this is here because tyscript throws error lines for chakra components like VStack */}
+            <VStack>
+              <Text fontSize={'15px'} p={1}>Limitation</Text>
+              <VStack fontFamily={'sans-serif arial'} lineHeight= '15px'>
+                <Text pl={'11px'} pr={'11px'}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Text>
+                <Text pl={'11px'} pr={'11px'}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Text>
+                <Text pl={'11px'} pr={'11px'}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Text>
+              </VStack>
+            </VStack>
+          </Box>          
         </HStack>
-        <Button w={'215px'} h={'42px'} borderRadius={'20px'} bgColor={'#000000'}>Access</Button>
+        <Button w={'215px'} h={'42px'} borderRadius={'20px'} bgColor={'#000000'} mt={'20.8px'} fontSize={'12px'}><Link href='/loginpage'>Access</Link></Button>
       </VStack>   
     </Box>
     </>
