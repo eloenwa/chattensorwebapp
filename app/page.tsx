68d68b23f2ef85a9e99f6b17ef833c78470b4b7a
eloenwa <eloenwa@users.noreply.github.com>
2023-02-21 13:24:28 -0600

    made home page responsive (mobile)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Box, HStack, VStack, Button, Text } from '@chakra-ui/react';
+import { Box, HStack, VStack, Button, Text, Stack } from '@chakra-ui/react';
 import Header from './Header';
 import Link from 'next/link';
 
@@ -12,12 +12,10 @@ export default function Home() {
       <Header />
       <Box bgColor={'#030B19'} display={'flex'} height="95vh" alignItems={'center'} justifyContent={'center'}
         color={'#FFFFFF'}>
-        {/* @ts-ignore  this is here because tyscript throws error lines for chakra components like VStack */}
-        <VStack>
-          <HStack fontSize={'12px'} w={'394px'} justifyContent={'space-between'}>
-            <Box h={'200px'} w={'187px'} borderWidth={"0.5px"} borderColor={'#707070'} borderRadius={'5px'} >
-              {/* @ts-ignore  this is here because tyscript throws error lines for chakra components like VStack */}
-              <VStack fontFamily={'Arial'}>
+        <VStack >
+          <Box display={'flex'} fontSize={'12px'} w={'394px'} justifyContent={'space-between'} flexDir={['column', 'row']} alignItems={'center'}>
+            <Box h={'200px'} w={'187px'} borderWidth={"0.5px"} borderColor={'#707070'} borderRadius={'5px'} mt={'10px !important'}>
+              <VStack fontFamily={'Arial'} >
                 <Text fontFamily={'Haffer-Regular'} fontSize={'15px'} p={1}>Capabilities</Text>
                 <VStack fontFamily={'sans-serif arial'} lineHeight='15px'>
                   <Text pl={'11px'} pr={'11px'}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Text>
@@ -26,8 +24,7 @@ export default function Home() {
                 </VStack>
               </VStack>
             </Box>
-            <Box h={'200px'} w={'187px'} borderWidth={"0.5px"} borderColor={'#707070'} borderRadius={'5px'} >
-              {/* @ts-ignore  this is here because tyscript throws error lines for chakra components like VStack */}
+            <Box h={'200px'} w={'187px'} borderWidth={"0.5px"} borderColor={'#707070'} borderRadius={'5px'} mt={'10px !important'} >
               <VStack>
                 <Text fontFamily={'Haffer-Regular'} fontSize={'15px'} p={1}>Limitation</Text>
                 <VStack fontFamily={'Arial'} lineHeight='15px'>
@@ -37,7 +34,7 @@ export default function Home() {
                 </VStack>
               </VStack>
             </Box>
-          </HStack>
+          </Box>
           <Link href='/loginpage'><Button w={'215px'} h={'42px'} borderRadius={'20px'} bgColor={'#000000'} mt={'20.8px'} fontSize={'12px'} fontWeight={'100'} fontFamily={'TT Commons Pro Mono Medium'}>ACCESS</Button></Link>
         </VStack>
       </Box>
