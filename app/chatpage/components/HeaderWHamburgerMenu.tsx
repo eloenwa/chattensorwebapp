6c44196ef3bf9e729e2b4c673e1aa0a948ff31b2
eloenwa <eloenwa@users.noreply.github.com>
2023-02-28 20:11:39 -0800

    in prog - mobile view - added elements to menu

--- app/chatpage/components/HeaderWHamburgerMenu.tsx
+++ app/chatpage/components/HeaderWHamburgerMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-    Box, HStack, Text, useDisclosure, Drawer,
+    Box, HStack, Text, useDisclosure, Input, Switch,
+    Drawer,
     DrawerBody,
     DrawerFooter,
     DrawerHeader,
@@ -9,12 +10,25 @@ import {
     DrawerCloseButton,
 } from '@chakra-ui/react';
 
+import { useRouter } from 'next/navigation';
+
+import LogOutIcon from '../../assets/logouticon.svg'
 
 //Hambuger menu for mobile view
 
 function HeaderWHamburgerMenu() {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
+
+    //initialize router to handle page redirects
+    const router = useRouter();
+
+    //This is the authentication function 
+    //this function will post a request to the backend to get a session key and route users to the chatpage upon successfull authentication
+    async function logOutFunc() {
+        console.log('logout'); //data contains email and password details 
+        router.push("/"); //upon authentication this routes users to the chattensor page
+    }
 
     return (
         <Box>
@@ -25,13 +39,34 @@ function HeaderWHamburgerMenu() {
             </HStack>
             <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
                 <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerHeader>NEW CHAT</DrawerHeader>
+                <DrawerContent p={-2}>
+                    <DrawerHeader>
+                        <Box w={'100%'} h={'50px'} display={'flex'} flexDir={'row'}
+                            alignItems={'center'} borderWidth={'0.25px'} borderColor={'#000000'} borderRadius={'4px'}>
+                            <Box w={'15px'} h={'15px'} bgColor={'pink'} m={'15px !important'}></Box>
+                            <Text color={'#000000'} fontFamily={'TT Commons Pro Mono Medium'} fontSize={'15px'}>NEW CHAT</Text>
+                        </Box>
+                    </DrawerHeader>
                     <DrawerBody>
-                        <p>CHAT 1</p>
+                        <Text>Hello</Text>
                     </DrawerBody>
-                    <DrawerFooter>
-                        <p>CHAT 1</p>
+                    <DrawerFooter display={'flex'} flexDir={'column'} alignItems={'flex-start'}>
+                        <Text fontFamily={'Haffer-Regular'} fontSize={'18px'}>Jacqueline Tasker-Steeves</Text>
+                        <Text fontFamily={'Haffer-Regular'} fontSize={'12px'}>jacquelinedawnts@gmail.com</Text>
+                        <Box mt={'30px !important'} alignItems={'center'} width={'100%'} display={'flex'} flexDir={'row'}>
+                            <Switch size={'sm'} justifySelf={'flex-start'} />
+                            <Text ml={'15px !important'} fontFamily={'Haffer-Regular'} fontSize={'12px'} w={'70%'} >Advanced Feedback</Text>
+                        </Box>
+                        <HStack mt={'30px !important'} alignItems={'center'} w={'100%'} >
+                            <Box h={'25px'} w={'25px'} display={'flex'} alignItems={'center'}>
+                                <Box h={'20px'} w={'20px'} justifySelf={'start'} _hover={{ borderWidth: '1px' }} onClick={() => logOutFunc()}>
+                                    <LogOutIcon />
+                                </Box>
+                            </Box>
+                            <Text ml={'15px !important'} fontFamily={'Haffer-Regular'} fontSize={'12px'} w={'70%'} >Log out</Text>
+                        </HStack>
+                        <Input w={'100%'} h={'50px'} fontFamily={'TT Commons Pro Mono Medium'} fontSize={'12px'} textAlign={'center'} color={'#000000'} placeholder='Enter UID'
+                            borderRadius={'4px'} borderWidth={'0.5px'} borderColor={'#000000'} mt={'40px !important'} />
                     </DrawerFooter>
                 </DrawerContent>
             </Drawer>
